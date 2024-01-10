5604395d16a22cb240c167f245caf36e37624116
Aanshu Dwiwedi <aanshu2dwiwedi@gmail.com>
2024-01-10 09:15:20 +0545

    Reduce array

--- array/array.js
+++ array/array.js
@@ -22,10 +22,31 @@ console.log({newGroup});
 // Write a function to find the maximum and minimum value of an array 
 //[22,1,33,9,25] => {min:1, max:55}
 
+const number = [22,1,33,9,25];
+const minMax = number.sort((a,b) => a - b);
+console.log({min: minMax[0] , max: minMax.pop()});
+
 // Write a program to exclude passsword from the array
 
 const users = [
     {user: "a", pw: "test"},
     {user: "b", pw: "test1"},
     {user: "c", pw: "test2"}
-]
+];
+
+// Immutable JS
+// JS methods that we use in array to do operations in a copy of the original data
+// Array
+// map, filter, reduce, some , find, every
+
+// Write a function that accepts an array and 
+// Return the sum of all the items of array
+
+// [1,2,3,4,5,6,7,8,9,10] => 55
+
+const number1 = [1,2,3,4,5,6,7,8,9,10];
+const sum = number1.reduce(
+(accumulator, currentValue) => accumulator + currentValue , 0,
+);
+
+console.log (sum);
