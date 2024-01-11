d9642193093a20f299276456858796e3cf4fafa2
Aanshu Dwiwedi <aanshu2dwiwedi@gmail.com>
2024-01-11 22:04:44 +0545

    Form update

--- html-css-js/index.js
+++ html-css-js/index.js
@@ -2,3 +2,13 @@ const sayHello = () => {
     const name =document.getElementById("name").value;
     alert (`Hello ${name}`);
 }
+
+const Date = () => {
+    const birthday = document.getElementById("birthday").value;
+    alert(`Date updated ${birthday}`);
+}
+
+const File = () => {
+    const myFile = document.getElementById("myFile").value;
+    alert(`File uploaded ${myFile}`);
+}
