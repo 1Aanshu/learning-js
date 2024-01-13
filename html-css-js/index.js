ad9523b23ce4fac9b0bfcdf7c371a408dd7f873f
Aanshu Dwiwedi <aanshu2dwiwedi@gmail.com>
2024-01-14 00:37:03 +0545

    update

--- html-css-js/index.js
+++ html-css-js/index.js
@@ -1,14 +1,16 @@
 const sayHello = () => {
     const name =document.getElementById("name").value;
     alert (`Hello ${name}`);
+    // radio
+    const gender = document.getElementsByName("gender")[0].checked ? document.getElementsByName("gender")[0].value:document.getElementsByName("gender")[1].value;
+    console.log(gender);
+
+    //select
+
+    
+    //file upload
+
+    //date manipulation
 }
 
-const Date = () => {
-    const birthday = document.getElementById("birthday").value;
-    alert(`Date updated ${birthday}`);
-}
 
-const File = () => {
-    const myFile = document.getElementById("myFile").value;
-    alert(`File uploaded ${myFile}`);
-}
