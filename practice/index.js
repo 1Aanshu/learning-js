1785d8c3e14893e0714316e00dbc79ba2c4268d3
Aanshu Dwiwedi <aanshu2dwiwedi@gmail.com>
2024-01-15 07:54:54 +0545

    To proper case

--- practice/index.js
+++ practice/index.js
@@ -1,30 +1,35 @@
 // Recap couple of concepts (array,array method chaining,spread Ops, Function)
-// Write a js function to convert the string in proper case 
+// Write a js function to convert the string in proper case
 
-const arr = [{name : "ram"}, {name : "sita"}, {name:"hari"}];
+const arr = [{ name: "ram" }, { name: "sita" }, { name: "hari" }];
 const first = arr[0];
 
-const [a,b,c,...rest] = arr;
+const [a, b, c, ...rest] = arr;
 
 // Immutable Js, Filter,map, reduce, every, some, find
 
-const isEveryValid =arr.every((item)=>item.name.split("")[0]==="r");
-const isSomeValid =arr.some((item)=> item.name.split(" ")[0]==="r");
-console.log({isEveryValid,isSomeValid});
+const isEveryValid = arr.every((item) => item.name.split(" ")[0] === "r");
+const isSomeValid = arr.some((item) => item.name.split(" ")[0] === "r");
+console.log({ isEveryValid, isSomeValid });
 
 // Function
 const checkArrayStartWithR = (ram) => {
-    return ram.every((item)=> item.name.split("")[0]==="r");
+  return ram.every((item) => item.name.split(" ")[0] === "r");
 };
 
 const fun = checkArrayStartWithR(arr);
-console.log({fun});
+console.log({ fun });
 
-// Write a js function to convert the string into the propercase 
+// Write a js function to convert the string into the propercase
 // Eg 'The fox is sly' => "The Fox Is Sly"
 
-const properCase =(string)=>{};
-properCase ("The fox is sly");
+const properCase = (string) => {
+    return string
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+};
+console.log(properCase("The fox is sly"));
 
 // Date and Date properties
 
@@ -33,13 +38,13 @@ console.log(today);
 
 const giveMeYear = today.getFullYear();
 const setMyYear = today.setFullYear(2023);
-console.log(today,giveMeYear,setMyYear);
+console.log(today, giveMeYear, setMyYear);
 
-const myToday = new Date (1705200098); //new Date()
-console.log({myToday});
+const myToday = new Date(1705200098); //new Date()
+console.log({ myToday });
 
 myToday.setDate(12);
-console.log({myToday});
+console.log({ myToday });
 
 // Practice month get-set, day get-set
 //Date Formats
